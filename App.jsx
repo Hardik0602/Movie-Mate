dafb1ccc078906b0b6d97bc6d509d55f223994be
hardik0602 <pathakhardik06@gmail.com>
2025-08-17 13:30:00 +0530

    .

--- App.jsx
+++ App.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Home from './src/screens/Home'
 import NotHome from './src/screens/NotHome'
 const Stack = createNativeStackNavigator();
 function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="NotHome" component={NotHome} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <GestureHandlerRootView className='flex-1'>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="NotHome" component={NotHome} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 export default App;
